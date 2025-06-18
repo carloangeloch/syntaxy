57e4a09e7c26acfe3fc80f86c31d5cd21b6a8925
Carlo Angelo Hilaus <carloangelo.ch@gmail.com>
2025-06-18 23:48:28 +0800

    CRITICAL FIX: Replacement of session pooling with PSQL DB, exit direct

--- backend/src/lib/db.py
+++ backend/src/lib/db.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 supabase: Client = create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_KEY'))
-DB_NAME = f'postgresql://postgres:{os.getenv('SQL_PWD')}@db.dpcpvqlqpmkbbcilbpij.supabase.co:5432/postgres'
+DB_NAME = f'postgresql://postgres.dpcpvqlqpmkbbcilbpij:{os.getenv('SQL_PWD')}@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 
 engine = create_engine(DB_NAME, echo=True)
 # engine = create_engine('sqlite:///database.db', echo=True)
